42cc268bdc1303e1406d3db473dcac3006fdb6f2
uran21 <uran21@mail.ru>
2022-05-04 23:40:01 +0300

    The blog changed

--- main.py
+++ main.py
@@ -14,12 +14,12 @@ import os
 
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'sqlite:///users.db'
+app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
 ckeditor = CKEditor(app)
 Bootstrap(app)
 
 ##CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("sqlite:///blog.db")
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///blog.db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
